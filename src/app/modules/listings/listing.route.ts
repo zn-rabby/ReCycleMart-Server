a137537b4a9ebcc6c8a85c1daac2dd19553f5205
rabby-web <rabby.webdeveloper@gmail.com>
2025-03-01 15:56:28 +0600

    feat: add validateRequest middleware using Zod for request validation

--- src/app/modules/listings/listing.route.ts
+++ src/app/modules/listings/listing.route.ts
@@ -1,9 +1,15 @@
 import express from 'express';
 import { listingController } from './listing.controller';
+import { listingValidation } from './listing.validation';
+import validateRequest from '../../middleware/validateRequest';
 
 const router = express.Router();
 
 // create listing
-router.post('/', listingController.createListing);
+router.post(
+  '/',
+  validateRequest(listingValidation.createListingValidationSchema),
+  listingController.createListing,
+);
 
 export const ListingRoutes = router;
