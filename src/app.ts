5eb2a81adadef5dfe741da00cd29f0a3903b899f
rabby-web <rabby.webdeveloper@gmail.com>
2025-03-01 15:37:39 +0600

    feat: add global error handler with support for Zod, validation, cast, and duplicate errors

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
 import router from './app/routes';
+import globalErrorHandler from './app/middleware/globalErrorHandler';
 
 const app: Application = express();
 
@@ -18,5 +19,7 @@ app.use('/api/v1', router);
 app.get('', (req: Request, res: Response) => {
   res.send('Server is running...');
 });
+
+app.use(globalErrorHandler);
 
 export default app;
